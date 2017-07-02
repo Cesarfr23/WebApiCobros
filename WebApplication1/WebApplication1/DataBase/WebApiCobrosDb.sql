804942a36ba078dfffa6200b8f63e5c585d1096e
Cesarfr23 <cesarfernandez860@gmail.com>
2017-07-01 20:40:21 -0400

    Agragando Data Base

---
+++ WebApplication1/WebApplication1/DataBase/WebApiCobrosDb.sql
@@ -0,0 +1,17 @@
+﻿use WebApiCobrosDb;
+
+create table Cobros
+(  
+   IdCobro int identity primary key,
+   Fecha datetime,
+   Referencia varchar(50),
+   IdRemoto int,
+   IdRuta int,
+   Mora money,
+   Monto Money,
+   Latitud float,
+   Longitud float,
+   EsNulo  varchar(19),
+
+
+);
