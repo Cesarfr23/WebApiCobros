fa009d29bc56a846dbc066e5c8f4c7a34cf860e4
Cesarfr23 <cesarfernandez860@gmail.com>
2017-07-02 15:13:18 -0400

    Cambios

---
+++ WebApplication1/WebApplication1/DAL/WebApiCobrosDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCobros.Models;
+
+namespace WebApiCobros.DAL
+{
+    public class WebApiCobrosDb : DbContext
+    {
+        public WebApiCobrosDb(DbContextOptions<WebApiCobrosDb> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cobros> Cobros { get; set; }
+    }
+}
